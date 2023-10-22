6d56f773bbda7764b375c5c88608859d4650e7a9
GusLed1870 <106112618+GusLed1870@users.noreply.github.com>
2023-10-22 14:11:41 -0300

    Update BrigadaData.java

--- src/Acceso_a_Datos/BrigadaData.java
+++ src/Acceso_a_Datos/BrigadaData.java
@@ -384,10 +384,9 @@ public class BrigadaData {
 
     public List<String> listarValoresSiniestros() {
         List<String> listaValores = new ArrayList<>();
-    String sql = "SELECT brigada.codBrigada as Nºbrigada, brigada.nombre_br as Nombre_Brigada, brigada.nro_cuartel, cuartel.nombre_cuartel, COUNT(siniestro.tipo) as Cant_Siniestros, AVG(siniestro.puntuacion) as Promedio_Puntuacion " +
-                 "FROM siniestro JOIN brigada ON brigada.codBrigada = siniestro.codBrigada JOIN cuartel ON brigada.nro_cuartel = cuartel.codCuartel " +
-                 "WHERE siniestro.puntuacion > 0 GROUP BY brigada.codBrigada ORDER BY AVG(siniestro.puntuacion) DESC";
-                
+        String sql = "SELECT brigada.codBrigada as Nºbrigada, brigada.nombre_br as Nombre_Brigada, brigada.nro_cuartel, cuartel.nombre_cuartel, COUNT(siniestro.tipo) as Cant_Siniestros, AVG(siniestro.puntuacion) as Promedio_Puntuacion "
+                + "FROM siniestro JOIN brigada ON brigada.codBrigada = siniestro.codBrigada JOIN cuartel ON brigada.nro_cuartel = cuartel.codCuartel "
+                + "WHERE siniestro.puntuacion > 0 GROUP BY brigada.codBrigada ORDER BY AVG(siniestro.puntuacion) DESC";
 
         try {
             PreparedStatement ps = con.prepareStatement(sql);
@@ -401,7 +400,7 @@ public class BrigadaData {
                 valores.append(rs.getString("nro_cuartel")).append(", ");
                 valores.append(rs.getString("nombre_cuartel")).append(", ");
                 valores.append(rs.getString("Cant_Siniestros")).append(", ");
-                
+
                 valores.append(rs.getString("Promedio_Puntuacion"));
 
                 listaValores.add(valores.toString());
@@ -414,35 +413,67 @@ public class BrigadaData {
         }
         return listaValores;
     }
-    
+
     public Map<Integer, Integer> obtenerCantidadSiniestrosPorMes(int año) {
-    Map<Integer, Integer> resultados = new HashMap<>();
-    ResultSet rs = null;
+        Map<Integer, Integer> resultados = new HashMap<>();
+        ResultSet rs = null;
 
-    try {
-        // Crear la consulta SQL
-        String sql = "SELECT MONTH(fecha_siniestro) AS Mes, COUNT(*) AS CantidadSiniestros " +
-                     "FROM siniestro " +
-                     "WHERE YEAR(fecha_siniestro) = ? " +
-                     "GROUP BY MONTH(fecha_siniestro)";
-        
-        // Crear la sentencia preparada
-        PreparedStatement ps = con.prepareStatement(sql);
-        ps.setInt(1, año);
+        try {
+            // Crear la consulta SQL
+            String sql = "SELECT MONTH(fecha_siniestro) AS Mes, COUNT(*) AS CantidadSiniestros "
+                    + "FROM siniestro "
+                    + "WHERE YEAR(fecha_siniestro) = ? "
+                    + "GROUP BY MONTH(fecha_siniestro)";
 
-        // Ejecutar la consulta
-        rs = ps.executeQuery();
+            // Crear la sentencia preparada
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, año);
 
-        // Almacenar los resultados en el Map
-        while (rs.next()) {
-            int mes = rs.getInt("Mes");
-            int cantidadSiniestros = rs.getInt("CantidadSiniestros");
-            resultados.put(mes, cantidadSiniestros);
+            // Ejecutar la consulta
+            rs = ps.executeQuery();
+
+            // Almacenar los resultados en el Map
+            while (rs.next()) {
+                int mes = rs.getInt("Mes");
+                int cantidadSiniestros = rs.getInt("CantidadSiniestros");
+                resultados.put(mes, cantidadSiniestros);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-    } catch (SQLException e) {
-        e.printStackTrace();
-    } 
-    return resultados;
-}
+        return resultados;
+    }
+
+    public List<String> listarValoresSiniestrosFinalizados(int yearfecha) {
+        List<String> listaValores = new ArrayList<>();
+        String sql = "SELECT brigada.codBrigada as Nºbrigada, brigada.nombre_br as Nombre_Brigada, brigada.nro_cuartel, cuartel.nombre_cuartel, COUNT(siniestro.tipo) as Cant_Siniestros, AVG(siniestro.puntuacion) as Promedio_Puntuacion "
+                + "FROM siniestro JOIN brigada ON brigada.codBrigada = siniestro.codBrigada JOIN cuartel ON brigada.nro_cuartel = cuartel.codCuartel "
+                + "WHERE siniestro.puntuacion > 0 AND YEAR(fecha_siniestro) = ? GROUP BY brigada.codBrigada ORDER BY AVG(siniestro.puntuacion) DESC";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, yearfecha); // Establece el año como parámetro
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                StringBuilder valores = new StringBuilder();
+                valores.append(rs.getString("Nºbrigada")).append(", ");
+                valores.append(rs.getString("Nombre_Brigada")).append(", ");
+                valores.append(rs.getString("nro_cuartel")).append(", ");
+                valores.append(rs.getString("nombre_cuartel")).append(", ");
+                valores.append(rs.getString("Cant_Siniestros")).append(", ");
+                valores.append(rs.getString("Promedio_Puntuacion"));
+
+                listaValores.add(valores.toString());
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla brigada " + ex.getMessage());
+        }
+        return listaValores;
+    }
 
 }
