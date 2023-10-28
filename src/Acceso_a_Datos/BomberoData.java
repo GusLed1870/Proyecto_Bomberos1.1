8043e5d53a8f2f17ebccdb16d65ee51ef197dfd9
leontwas <140932426+leontwas@users.noreply.github.com>
2023-10-28 01:45:12 -0300

    Trabajé en la vista_siniestro

--- src/Acceso_a_Datos/BomberoData.java
+++ src/Acceso_a_Datos/BomberoData.java
@@ -26,27 +26,28 @@ public class BomberoData {
         int cantidadBomberosEnBrigada = contarBomberosEnBrigada(brigadaId);
         if (cantidadBomberosEnBrigada >= 5) {
             JOptionPane.showMessageDialog(null, "La brigada ya tiene 5 bomberos. Debe asignar otra brigada.");
-            return;
-        }
-        String sql = "INSERT INTO bombero (dni, nombre_ape, fecha_nac, celular, codBrigada, grupoSanguineo, estado) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        try {
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, bombero.getDni());
-            ps.setString(2, bombero.getNombre_ape());
-            ps.setDate(3, Date.valueOf(bombero.getFecha_nac()));
-            ps.setLong(4, bombero.getCelular());
-            ps.setInt(5, brigadaId);
-            ps.setString(6, bombero.getGrupoSanguineo());
-            ps.setBoolean(7, bombero.isEstado());
-            ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                bombero.setId_bombero(rs.getInt(1));
-                JOptionPane.showMessageDialog(null, "Bombero añadido con éxito. Legajo: " + bombero.getId_bombero());
+        } else {
+            String sql = "INSERT INTO bombero (dni, nombre_ape, fecha_nac, celular, codBrigada, grupoSanguineo, estado) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            try {
+                PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, bombero.getDni());
+                ps.setString(2, bombero.getNombre_ape());
+                ps.setDate(3, Date.valueOf(bombero.getFecha_nac()));
+                ps.setLong(4, bombero.getCelular());
+                ps.setInt(5, brigadaId);
+                ps.setString(6, bombero.getGrupoSanguineo());
+                ps.setBoolean(7, bombero.isEstado());
+                ps.executeUpdate();
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    bombero.setId_bombero(rs.getInt(1));
+                    JOptionPane.showMessageDialog(null, "Bombero añadido con éxito. Legajo: " + bombero.getId_bombero());
+                }
+
+                ps.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al guardar el bombero: " + ex.getMessage());
             }
-            ps.close();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al guardar el bombero: " + ex.getMessage());
         }
     }
 
